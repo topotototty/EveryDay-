f429949c3ab2c41bd344fc04772b9f545e7ada6a
topototty <myshmyshevskaya@gmail.com>
2023-02-20 21:09:01 +0300

    MEEEW

---
+++ Dairy/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Dairy
+{
+    public partial class MainWindow : Window
+    {
+        string name;
+        string description;
+        BindingList<Note> All_Notes;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            All_Notes = Note.Deserialize();
+
+            if (All_Notes == null)
+            {
+                All_Notes = new BindingList<Note>();
+            }
+
+            Select_Date.SelectedDate= DateTime.Now;
+            Note.Serialize(All_Notes);
+            Show();
+        }
+
+        private void Create(object sender, RoutedEventArgs e)
+        {
+            
+            All_Notes = Note.Deserialize();
+            name = Note_Name.Text;
+            description = Note_Description.Text;
+
+            Note New_Note = new Note() { Name = name, Description = description, Date = Select_Date.SelectedDate.Value};
+            if (All_Notes == null)
+            {
+                All_Notes = new BindingList<Note> { New_Note };
+            }
+            else
+            {
+                All_Notes.Add(New_Note);
+            }
+            Note.Serialize(All_Notes);
+            Show();
+        }
+
+        private void Show()
+        {
+            All_Notes = Note.Deserialize();
+            BindingList<Note> Dayly_Notes = new BindingList<Note>();
+            foreach (Note note in All_Notes)
+            {
+                if (note.Date == Select_Date.SelectedDate)
+                {
+                    Dayly_Notes.Add(note);
+                }
+            }
+
+            Note.Serialize(All_Notes);
+
+            View_Notes.ItemsSource = Dayly_Notes;
+        }
+
+
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            foreach (Note note in All_Notes)
+            {
+                if (View_Notes.SelectedItem == note)
+                {
+                    note.Name = Note_Name.Text;
+                    note.Description = Note_Description.Text;
+                    break;
+                }
+            }
+            Note.Serialize(All_Notes);
+            Show();
+        }
+
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+            foreach (Note note in All_Notes)
+            {
+                if (View_Notes.SelectedItem == note)
+                {
+                    All_Notes.Remove(note);
+                    break;
+                }
+            }
+            
+            Note.Serialize(All_Notes);
+            Show();
+        }
+
+        private void Selected_Date_Chaneged(object sender, SelectionChangedEventArgs e)
+        {
+            Show();
+        }
+    }
+}
---
+++ Dairy/Note.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Dairy
+{
+    class Note
+    {
+        private string _Name;
+        private string _Description;
+        public DateTime Date { get; set; }
+
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+        public string Description
+        {
+            get { return _Description; }
+            set { _Description = value; }
+        }
+        public static void Serialize(BindingList<Note> notes)
+        {
+            string json = JsonConvert.SerializeObject(notes);
+            File.WriteAllText("C://Users//futbo//OneDrive//Рабочий стол//Dairy//About_Notes.json", json);
+        }
+
+        public static BindingList<Note> Deserialize()
+        {
+            string About_Notes = File.ReadAllText("C://Users//futbo//OneDrive//Рабочий стол//Dairy//About_Notes.json");
+            BindingList<Note> notes = JsonConvert.DeserializeObject<BindingList<Note>>(About_Notes);
+            return notes;
+        }
+    }
+}
